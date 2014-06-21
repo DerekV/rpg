0f547b793d62837d93860807bf2744a8bbdfe7b9
Derek VerLee <derekverlee@gmail.com>
2014-06-21 17:03:35 -0400

    The monster fights back

--- Main.hs
+++ Main.hs
@@ -22,16 +22,24 @@ playerDeck :: ActionDeck
 playerDeck = [Action "poke" 1, Action "bite" 1, Action "slap" 1, Action "trip" 1,
               Action "claw" 1, Action "punch" 2, Action "kick" 3, Action "taunt" 0]
 
+monsterDeck :: ActionDeck
+monsterDeck = [Action "gurgle" 0, Action "bite" 2, Action "pounce" 2,
+               Action "paw" 1, Action "howl" 0, Action "whimper" 0 ]
+
 main :: IO ()
 main = do
+  print "Welcome, adventurer."
+  print "You have enountered a foul beast!"
   playGame $ EnemyHitpoints 5
 
 playGame :: GameState -> IO ()
 playGame (EnemyHitpoints enemyHitpoints) = do
-  hand <- shuffleAndDraw 1 playerDeck
-  print hand
+  monsterAction <- head <$> shuffleAndDraw 1 monsterDeck
+  print $ "It " ++ show monsterAction ++ "s!"
+  playerHand <- shuffleAndDraw 1 playerDeck
+  print playerHand
   print $ "The monster has " ++ show enemyHitpoints ++ " hitpoints left"
   getLine
   if (enemyHitpoints > 0)
-    then playGame $ EnemyHitpoints (enemyHitpoints - damage (head hand));
+    then playGame $ EnemyHitpoints (enemyHitpoints - damage (head playerHand));
     else print "You have defeated the monster!  Good bye, mighty hero"
