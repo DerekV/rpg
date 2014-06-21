2c00508269d523c79c1f4254ebaa493417ed6160
Derek VerLee <derekverlee@gmail.com>
2014-06-21 16:52:46 -0400

    Different attacks do different amounts of damage

--- Main.hs
+++ Main.hs
@@ -5,8 +5,22 @@ import Data.Random.RVar (runRVar)
 
 data GameState = EnemyHitpoints Int
 
-shuffleAndDraw :: Int -> [a] -> IO [a]
+type Damage = Int
+data Action = Action {
+  verb::String,
+  damage::Damage}
+instance Show Action where
+  show action = verb action
+
+
+type ActionDeck = [Action]
+
+shuffleAndDraw :: Int -> [Action] -> IO [Action]
 shuffleAndDraw n deck = runRVar ((take n) <$>  shuffle deck) DevURandom
+
+playerDeck :: ActionDeck
+playerDeck = [Action "poke" 1, Action "bite" 1, Action "slap" 1, Action "trip" 1,
+              Action "claw" 1, Action "punch" 2, Action "kick" 3, Action "taunt" 0]
 
 main :: IO ()
 main = do
@@ -14,11 +28,10 @@ main = do
 
 playGame :: GameState -> IO ()
 playGame (EnemyHitpoints enemyHitpoints) = do
-  hand <- shuffleAndDraw 1 ["poke", "bite", "slap", "trip", "claw", "punch", "kick", "taunt"]
+  hand <- shuffleAndDraw 1 playerDeck
   print hand
   print $ "The monster has " ++ show enemyHitpoints ++ " hitpoints left"
   getLine
-  if (enemyHitpoints > 0) then do
-    playGame $ EnemyHitpoints (enemyHitpoints - 1);
-    else
-    print "You have defeated the monster!  Good bye, mighty hero"
+  if (enemyHitpoints > 0)
+    then playGame $ EnemyHitpoints (enemyHitpoints - damage (head hand));
+    else print "You have defeated the monster!  Good bye, mighty hero"
