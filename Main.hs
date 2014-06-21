440f48580a753bbee172bc7a89006bc853f4ce9d
Derek VerLee <derekverlee@gmail.com>
2014-06-21 16:35:34 -0400

    The monster dies at 0, triggering a win

--- Main.hs
+++ Main.hs
@@ -1,8 +1,6 @@
 import Data.Functor
 import Data.Random.Extras (shuffle)
 import Data.Random.Source.DevRandom
---import Data.Random.Source (RandomSource)
---import Data.Random.Internal
 import Data.Random.RVar (runRVar)
 
 data GameState = EnemyHitpoints Int
@@ -10,14 +8,17 @@ data GameState = EnemyHitpoints Int
 shuffleAndDraw :: Int -> [a] -> IO [a]
 shuffleAndDraw n deck = runRVar ((take n) <$>  shuffle deck) DevURandom
 
-main :: IO b
+main :: IO ()
 main = do
   playGame $ EnemyHitpoints 5
 
-playGame :: GameState -> IO b
+playGame :: GameState -> IO ()
 playGame (EnemyHitpoints enemyHitpoints) = do
   hand <- shuffleAndDraw 1 ["poke", "bite", "slap", "trip", "claw", "punch", "kick", "taunt"]
   print hand
   print $ "The monster has " ++ show enemyHitpoints ++ " hitpoints left"
   getLine
-  playGame $ EnemyHitpoints (enemyHitpoints - 1);
+  if (enemyHitpoints > 0) then do
+    playGame $ EnemyHitpoints (enemyHitpoints - 1);
+    else
+    print "You have defeated the monster!  Good bye, mighty hero"
