e105443b7c08f4f0cd04a3e35773262738c4cc74
Derek VerLee <derekverlee@gmail.com>
2014-06-22 11:18:40 -0400

    Rewrite code in a less monolythic style, improve output

--- Main.hs
+++ Main.hs
@@ -1,10 +1,12 @@
-import Control.Monad
+import Control.Monad hiding (mapM_)
+import Control.Monad.State
 import Data.Functor
+import Data.Foldable (any, mapM_)
+import Data.Sequence as Sq (Seq, length, fromList, index, update)
 import Data.Random.Extras (shuffle)
 import Data.Random.Source.DevRandom
 import Data.Random.RVar (runRVar)
 
-data GameState = GameState {playerHp :: Int, monsterHp :: Int};
 
 type Damage = Int
 data Action = Action {
@@ -14,13 +16,34 @@ instance Show Action where
   show action = verb action
 
 
-type ActionDeck = [Action]
-
 shuffleAndDraw :: Int -> [Action] -> IO [Action]
 shuffleAndDraw n deck = runRVar ((take n) <$>  shuffle deck) DevURandom
 
+randomAction :: [Action] -> IO Action
+randomAction as = head <$> shuffleAndDraw 1 as
+
+data Character = Character { name :: String,
+                             hpCurrently :: Int,
+                             hpTotal :: Int,
+                             ioAction :: IO Action }
+
+player :: Character
+player = Character { name="Player",
+                     hpCurrently=5,
+                     hpTotal=5,
+                     ioAction=(getLine>>randomAction playerDeck)}
+monster = Character { name="MonsterBeast",
+                      hpCurrently=5,
+                      hpTotal=5,
+                      ioAction=randomAction monsterDeck}
+
+data Game = Game { characters :: Seq Character, turn :: Int }
+
+
+type ActionDeck = [Action]
+
 playerDeck :: ActionDeck
-playerDeck = [Action "poke" 1, Action "bite" 1, Action "slap" 1, Action "trip" 1,
+playerDeck = [Action "poke" 1, Action "stab" 3, Action "slap" 1, Action "trip" 1,
               Action "claw" 1, Action "punch" 2, Action "kick" 3, Action "taunt" 0]
 
 monsterDeck :: ActionDeck
@@ -31,38 +54,58 @@ main :: IO ()
 main = do
   putStrLn "Welcome, adventurer."
   putStrLn "You have enountered a foul beast!"
-  playGame $ GameState 5 5
+  playGame $ Game {characters= fromList [monster, player], turn=0}
 
-playGame :: GameState -> IO ()
-playGame initialState = do
-  getLine
-  afterMonsterGoes <- monsterTurn initialState
-  when (playerHp initialState > playerHp afterMonsterGoes) $
-    printPlayerStatus afterMonsterGoes
-  if (playerHp afterMonsterGoes <= 0) then
-    putStrLn "You are dead and soon forgotten."
-    else do
-      newState <- playerTurn afterMonsterGoes
-      if (monsterHp newState <= 0)
-        then putStrLn "You have defeated the monster!  Good bye, mighty hero"
-        else playGame newState
+playGame :: Game -> IO ()
+playGame startOfTurn = do
+  tellStartTurn startOfTurn
+  afterThisTurn <- doTurn startOfTurn
+  tellTurnEndResults startOfTurn afterThisTurn
+  if (gameOver afterThisTurn)
+    then return()
+    else playGame (nextTurn afterThisTurn)
 
-monsterTurn :: GameState -> IO(GameState)
-monsterTurn (GameState php mhp) = do
-  monsterAction <- head <$> shuffleAndDraw 1 monsterDeck
-  putStrLn $ "It " ++ show monsterAction ++ "s!"
-  let newPhp = php - damage monsterAction
-  return (GameState newPhp mhp)
+gameOver :: Game -> Bool
+gameOver game = Data.Foldable.any (\c -> hpCurrently c <= 0 ) (characters game)
 
-playerTurn :: GameState -> IO(GameState)
-playerTurn (GameState php mhp) = do
-  playerAction <- head <$> shuffleAndDraw 1 playerDeck
-  putStrLn $ "You " ++ (show playerAction) ++ " the monster."
-  let newMhp = mhp - damage playerAction
-  putStrLn $ "The monster has " ++ show newMhp ++ " hitpoints left."
-  return (GameState php newMhp)
+nextTurn :: Game -> Game
+nextTurn game = game { turn=mod (1 + turn game) (Sq.length $ characters game)}
 
-printPlayerStatus :: GameState -> IO()
-printPlayerStatus state = do
-  putStrLn $ "You have " ++ show hp ++ " hitpoints left"
-  where hp = playerHp state
+getTurnTaker :: Game -> Character
+getTurnTaker game = index (characters game) (turn game)
+
+tellStartTurn :: Game -> IO()
+tellStartTurn game = do
+  let character = getTurnTaker game
+  putStrLn $ "Start of " ++ name character ++ "'s turn"
+
+tellTurnEndResults :: Game -> Game -> IO ()
+tellTurnEndResults start end = do
+  let character = getTurnTaker start
+  putStrLn $ (name character) ++ "'s turn ends";
+  Data.Foldable.mapM_ (tellHpRemaining) (characters end)
+  putStrLn ""
+  return ()
+
+tellHpRemaining :: Character -> IO ()
+tellHpRemaining x = do
+  putStrLn $ name x ++ ": " ++ (show . hpCurrently) x ++ " remaining."
+
+doTurn :: Game -> IO Game
+doTurn startOfTurn = do
+  let
+    pos = turn startOfTurn
+    cs = characters startOfTurn
+    character = index cs  pos
+    targetPos = mod (1+pos) (Sq.length cs)
+    target = index cs targetPos
+  action <- ioAction character
+  putStrLn $ name character
+    ++ " " ++ verb action ++ "s at " ++ name target
+    ++ " for " ++ (show . damage) action ++ " damage.";
+  return startOfTurn {characters = update targetPos
+                                   (applyDamage (damage action) target) cs }
+
+applyDamage :: Int->Character->Character
+applyDamage dmg character =
+  character { hpCurrently = hpCurrently character - dmg }
